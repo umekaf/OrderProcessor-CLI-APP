463eb1dfaaf30b89842ad34e9e6594e020b50368
umekaf <kafleumesh@gmail.com>
2022-06-21 02:12:38 +0200

    Creaed a CLI file to take input from the user

--- lib/order_processor/cli.rb
+++ lib/order_processor/cli.rb
@@ -2,5 +2,11 @@
 
 module OrderProcessor
   class Cli
+    loop do
+      puts "Enter the item you like to add. (type 'stop' to exit purchase)"
+      input = gets.chomp
+      user_input = input.upcase
+      break if user_input == 'STOP'
+    end
   end
 end
