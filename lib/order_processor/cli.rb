3fede7602f74ae8dfcb69bd36f2e95ba148ac4ea
umekaf <kafleumesh@gmail.com>
2022-06-21 02:29:31 +0200

    Added logic to print the final ordered items codes with total price

--- lib/order_processor/cli.rb
+++ lib/order_processor/cli.rb
@@ -24,19 +24,26 @@ module OrderProcessor
       elsif user_input == 'CF1'
         coffee_count += 1
       end
-
-      if green_tea_count.even?
-        green_tea_count.times { ordered_items << Product.new('GR1', 'green tea', 3.11 / 2) }
-      else
-        (green_tea_count - 1).times { ordered_items << Product.new('GR1', 'green tea', 1.55) }
-        ordered_items << Product.new('GR1', 'green tea', 3.11)
-      end
-
-      strawberry_price = strawberry_count >= 3 ? 4.50 : 5.00
-      strawberry_count.times { ordered_items << Product.new('SR1', 'strawberry', strawberry_price) }
-
-      coffee_price = coffee_count >= 3 ? (11.23 * 2 / 3) : 11.23
-      coffee_count.times { ordered_items << Product.new('CF1', 'coffee', coffee_price) }
     end
+
+    if green_tea_count.even?
+      green_tea_count.times { ordered_items << Product.new('GR1', 'green tea', 3.11 / 2) }
+    else
+      (green_tea_count - 1).times { ordered_items << Product.new('GR1', 'green tea', 1.55) }
+      ordered_items << Product.new('GR1', 'green tea', 3.11)
+    end
+
+    strawberry_price = strawberry_count >= 3 ? 4.50 : 5.00
+    strawberry_count.times { ordered_items << Product.new('SR1', 'strawberry', strawberry_price) }
+
+    coffee_price = coffee_count >= 3 ? (11.23 * 2 / 3) : 11.23
+    coffee_count.times { ordered_items << Product.new('CF1', 'coffee', coffee_price) }
+
+    print 'Basket'
+    sum = ordered_items.inject(0) do |sum, item|
+      print " #{item.product_code},"
+      sum += item.price
+    end
+    print "Total: #{sum}"
   end
 end
